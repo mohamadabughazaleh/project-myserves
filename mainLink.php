626fcd789cc284a512953e96d48e61c9d4d902c9
hussain <hussainabuqattam3@gmail.com>
2021-08-04 10:33:33 -0700

    Link for new project

---
+++ mainLink.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <!-- IE Compatibility Meta -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- First Mobile Meta -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="layot/css/bootstrap.css">
+    <link rel="stylesheet" href="layot/css/animate.css">  
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link rel="stylesheet" href="layot/css/main.css">
+    <link rel="stylesheet" href="layot/css/normalize.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link href="https://fonts.googleapis.com/css2?family=Bad+Script&family=Cookie&family=Lobster&family=Permanent+Marker&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+</head>
+</html>
+<body>
+
+        <script src="layot/js/jquery-3.5.1.min.js"></script>
+        <script src="layot/js/popper.min.js"></script>
+        <script src="layot/js/bootstrap.min.js"></script>
+        <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+        <script src="layot/js/main.js"></script>
+        <script src="layot/js/wow.min.js"></script>
+        <script>
+          AOS.init();
+        </script>
+        <script>new WOW().init();</script>
+</body>
