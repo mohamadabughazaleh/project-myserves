08af0c250e6de8e506fe6d3bf0d57b165a6d14a2
mohamadabughazaleh <mohamadabughazaleh57@gmail.com>
2021-08-11 23:09:48 +0300

    ghazal

--- section.php
+++ section.php
@@ -6,12 +6,13 @@
             <div class="col-lg col-md col-sm col-xs">
                 <button class="btn  dropdown-toggle " type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">خدمات مميزه </button>
                     <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                        <a class="dropdown-item" href="#">خدمات مميزة</a>
+                        <a class="dropdown-item" href="#0">خدمات مميزة</a>
                         <a class="dropdown-item" href="#">أضيفيت حديثا</a>
                         <a class="dropdown-item" href="#">خدمات صاعدة</a>
                     </div>
                     <h1 class="colarname">أعمال</h1>
                     <p>احصل على الدعم اللازم لتسيير أعمالك بشكل أكثر سلاسة</p>
+
             </div>
         </div>
     </div>
@@ -25,7 +26,7 @@
                 <div class="col-lg-4 col-md-6 col-sm-6  col-xs">
                     <div class="card">
                         <div class="img">
-                            <img class="card-img-top" src="../pexels-fauxels-3184418 (1).jpg" alt="Card image cap">
+                            <img class="card-img-top" src="../project-myserves/layot/imge/pexels-fauxels-3184418 (1).jpg" alt="Card image cap">
                         </div>
                         <div class="visit-wibsite">
                             <ul>
@@ -51,7 +52,7 @@
                 <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
                     <div class="card">
                         <div class="img">
-                            <img class="card-img-top" src="../pexels-energepiccom-159888.jpg" alt="Card image cap">
+                            <img class="card-img-top" src="../project-myserves/layot/imge/pexels-energepiccom-159888.jpg" alt="Card image cap">
                         </div>
                         <div class="visit-wibsite">
                             <ul>
@@ -77,7 +78,7 @@
                 <div class="col-lg-4 col-md-6 col-sm-6 col-xs">
                     <div class="card">
                         <div class="img">
-                            <img class="card-img-top" src="../pexels-fauxels-3184292.jpg" alt="Card image cap">
+                            <img class="card-img-top" src="../project-myserves/layot/imge/pexels-fauxels-3184292.jpg" alt="Card image cap">
                         </div>
                         <div class="visit-wibsite">
                             <ul>
@@ -104,7 +105,7 @@
                 <div class="col-lg-4 col-md-6 col-sm-6 col-xs">
                     <div class="card">
                         <div class="img">
-                            <img class="card-img-top" src="../pexels-anamul-rezwan-1216544 (1).jpg" alt="Card image cap">
+                            <img class="card-img-top" src="../project-myserves/layot/imge/pexels-anamul-rezwan-1216544 (1).jpg" alt="Card image cap">
                         </div>
                         <div class="visit-wibsite">
                             <ul>
@@ -130,7 +131,7 @@
                 <div class="col-lg-4 col-md-6 col-sm-6  col-xs">
                     <div class="card">
                         <div class="img">
-                            <img class="card-img-top" src="../pexels-malte-luk-2244746.jpg" alt="Card image cap">
+                            <img class="card-img-top" src="../project-myserves/layot/imge/pexels-malte-luk-2244746.jpg" alt="Card image cap">
                         </div>
                         <div class="visit-wibsite">
                             <ul>
@@ -156,7 +157,7 @@
                 <div class="col-lg-4 col-md-6 col-sm-6 col-xs">
                     <div class="card">
                         <div class="img">
-                            <img class="card-img-top" src="../pexels-jeshootscom-834892.jpg" alt="Card image cap">
+                            <img class="card-img-top" src="../project-myserves/layot/imge/pexels-jeshootscom-834892.jpg" alt="Card image cap">
                         </div>
                         <div class="visit-wibsite">
                             <ul>
