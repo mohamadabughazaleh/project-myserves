c7f06412739e8cae4e6c1cd0eb70153a86f7e8fe
hussain <hussainabuqattam3@gmail.com>
2021-08-20 10:48:43 -0700

    section-hussain

---
+++ serves-section.php
@@ -0,0 +1,125 @@
+<?php include ("topnav.php")?>
+
+
+
+<h1 class="name-section">قسم الخدمات</h1>
+
+<!--start section-->
+<div class="container">
+    <div class="row">
+        <div class="col-md-3">
+            <div class="container-section-freelance">
+              <a href="#" class="section-title">كهرباء</a>
+              <hr>
+              <ul>
+                 <li><a href="#">اخصائي كهربائي منازل</a></li>
+                 <li><a href="#">كهربائي مشاريع</a></li>
+                 <li><a href="#">مهندس كهرباء</a></li>
+                 <li><a href="#">اخصائي طاقة شمسية</a></li>
+                 <li><a href="#">صيانة محركات كهربائية</a></li>
+                 <li><a href="#">اخرى ...</a></li>
+              </ul>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="container-section-freelance">
+            <a href="#" class="section-title">صيانة مركبات</a>
+              <hr>
+              <ul>
+                 <li><a href="#">ميكانيكي عام</a></li>
+                 <li><a href="#">كهربائي مركبات </a></li>
+                 <li><a href="#">دهان مركبات</a></li>
+                 <li><a href="#">تنجيد فرش المركبات </a></li>
+                 <li><a href="#">صيانة مركبات الهايبرد</a></li>
+                 <li><a href="#">تعديل مركبات </a></li>
+                 <li><a href="#">اخرى ...</a></li>
+
+              </ul>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="container-section-freelance">
+            <a href="#" class="section-title">صرف الصحي</a>
+              <hr>
+              <ul>
+                 <li><a href="#">صيانة امدادات الصرف الصحي</a></li>
+                 <li><a href="#">فني تمديد مشاريع الصرف الصحي</a></li>
+                 <li><a href="#">قطع صرف الصحي</a></li>
+                 <li><a href="#">تركيب مضخات مياه</a></li>
+                 <li><a href="#">تركيب تمديدات الطاقة الشمسية</a></li>
+                 <li><a href="#">اخرى ...</a></li>
+              </ul>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="container-section-freelance">
+            <a href="#" class="section-title">فلاتر مياه</a>
+              <hr>
+              <ul>
+                 <li><a href="#">تركيب فلاتر مياه</a></li>
+                 <li><a href="#">صيانة فلاتر</a></li>
+                 <li><a href="#">تركيب محطات فلاتر ضخمة</a></li>
+                 <li><a href="#">قطع فلاتر</a></li>
+                 <li><a href="#">اخرى ...</a></li>
+              </ul>
+            </div>
+        </div>
+        <hr>
+        <div class="col-md-3">
+            <div class="container-section-freelance">
+            <a href="#" class="section-title">اعمار وبناء</a>
+              <hr>
+              <ul>
+                 <li><a href="#">مهندس واخصائي مشاريع</a></li>
+                 <li><a href="#">عمال مشاريع</a></li>
+                 <li><a href="#">تمديد بلاط </a></li>
+                 <li><a href="#">قطع وادوات اعمار </a></li>
+                 <li><a href="#">اخرى ...</a></li>
+              </ul>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="container-section-freelance">
+            <a href="#" class="section-title">اثاث </a>
+              <hr>
+              <ul>
+                 <li><a href="#">غرف نوم </a></li>
+                 <li><a href="#">ابواب خشب</a></li>
+                 <li><a href="#">ثلاجات </a></li>
+                 <li><a href="#">فرن منزلي</a></li>
+                 <li><a href="#">اطقم كنب</a></li>
+                 <li><a href="#">مكاتب شركات</a></li>
+                 <li><a href="#">اخرى ...</a></li>
+              </ul>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="container-section-freelance">
+            <a href="#" class="section-title">صيانة ادوات منزلية</a>
+              <hr>
+              <ul>
+                 <li><a href="#"> فني صيانة ثلاجات</a></li>
+                 <li><a href="#"> فني صيانة حاسوب وهواتف </a></li>
+                 <li><a href="#"> فني صيانة افرن الغاز</a></li>
+                 <li><a href="#">فني صيانة شاشات التلفاز</a></li>
+                 <li><a href="#">فني مكيفات ومراوح</a></li>
+                 <li><a href="#">اخرى ...</a></li>
+              </ul>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="container-section-freelance">
+            <a href="#" class="section-title">خدمات اخرى</a>
+              <hr>
+              <ul>
+                 <li><a href="#">فني نجارة</a></li>
+                 <li><a href="#">فني حدادة</a></li>
+                 <li><a href="#">فني مطابخ</a></li>
+                 <li><a href="#">فني تنجيد </a></li>
+                 <li><a href="#">اخرى ...</a></li>
+              </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<!--start section-->
