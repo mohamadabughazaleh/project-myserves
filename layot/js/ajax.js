813e8ead8084296c5272f8bf032261eba934a7a5
hussain <hussainabuqattam3@gmail.com>
2021-09-22 13:00:47 -0700

    vv

--- layot/js/ajax.js
+++ layot/js/ajax.js
@@ -1,44 +1,19 @@
 //add serves 
-//freelancer add
-$('#freelancer-add').click(function(){
+$(document).ready(function(){
   
-    $(document).ready(function(){
-        $('#add_frelancer_main').change(function(){
-            var main_frelancers = $(this).val();
-            if(main_frelancers){
-                $.get(
-                    "ajax.php",
-                    {main_frelancer_add: main_frelancers},
-                    function(data){
-                        $('#add-frelancer-sub').html(data);
-                    }
+    $('#add_main_select').change(function(){
+        var main_select = $(this).val();
+        if(main_select){
+            $.get(
+                "ajax.php",
+                {main_selector : main_select},
+                function(data){
+                    $('#add_sub_select').html(data);
+                }
 
-                );
-            }else{
-                $('#add-frelancer-sub').html('<option>اختر الفرع الرئيسي اولا</option>');
-            }
-        })
-    })
-});
-//servess add
-
-$('#servess-add').click(function(){
-  
-    $(document).ready(function(){
-        $('#add_serves_main').change(function(){
-            var main_servess = $(this).val();
-            if(main_servess){
-                $.get(
-                    "ajax.php",
-                    {main_servess_add: main_servess},
-                    function(data){
-                        $('#add_serves_sub').html(data);
-                    }
-
-                );
-            }else{
-                $('#add_serves_sub').html('<option>اختر الفرع الرئيسي اولا</option>');
-            }
-        })
+            );
+        }else{
+            $('#add_sub_select').html('<option>اختر الفرع الرئيسي اولا</option>');
+        }
     })
 });
