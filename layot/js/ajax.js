be829bc915ae033854c68f8929f9acbe2edf1b16
Hamzaabuqwaider <hamzaqwaider123@gmail.com>
2021-09-27 00:19:49 +0300

    hamza

--- layot/js/ajax.js
+++ layot/js/ajax.js
@@ -3,14 +3,15 @@ $(document).ready(function(){
   
     $('#add_main_select').change(function(){
         var main_select = $(this).val();
+
         if(main_select){
             $.get(
                 "ajax.php",
                 {main_selector : main_select},
                 function(data){
                     $('#add_sub_select').html(data);
+                    console.log(data);
                 }
-
             );
         }else{
             $('#add_sub_select').html('<option>اختر الفرع الرئيسي اولا</option>');
